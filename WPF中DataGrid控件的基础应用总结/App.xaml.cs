3803ebc6b5e9f022c6f4067735dc7ad1949cf418
121231982 <121231982@163.com>
2018-12-26 15:38:55 +0800

    初次提交

---
+++ WPF中DataGrid控件的基础应用总结/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF中DataGrid控件的基础应用总结
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ WPF中DataGrid控件的基础应用总结/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF中DataGrid控件的基础应用总结
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ObservableCollection<Customer> list = InitCustomerData.InitData();  // 向容器中添加数据
+            this.DG1.DataContext = list;
+        }
+    }
+    public class InitCustomerData
+    {
+       
+        
+    
+internal static ObservableCollection<Customer> InitData()
+        {
+            ObservableCollection<Customer> list = new ObservableCollection<Customer>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Customer customer = new Customer();
+                customer.FirstName = "a"+i.ToString ();
+                customer.LastName = "a" + i.ToString();
+                 UriBuilder uri;
+                 uri = new UriBuilder("http://b.c/d/e.f");
+
+                 customer.Email = uri.Uri;
+                customer.IsMember = false;
+                list.Add(customer);
+
+            } 
+
+            return list;
+}
+    
+    }
+    public class Customer
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public Uri Email { get; set; }
+    public bool IsMember { get; set; }
+}
+
+}
